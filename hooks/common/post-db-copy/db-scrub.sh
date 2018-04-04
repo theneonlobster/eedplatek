6fb24a79ed50cb38998c91f86d6289e4798f33f1
David Platek <theneonlobster@gmail.com>
2018-04-04 10:54:40 -0700

    TNL-000: Change the post-db-copy Cloud Hook to remove the scrubbing operation because it causes errors.

--- hooks/common/post-db-copy/db-scrub.sh
+++ hooks/common/post-db-copy/db-scrub.sh
@@ -15,8 +15,8 @@ source_env="$4"
 
 acsf_file="/mnt/files/$AH_SITE_GROUP.$AH_SITE_ENVIRONMENT/files-private/sites.json"
 if [ ! -f $acsf_file ]; then
-  echo "$site.$target_env: Scrubbing database $db_name"
-  drush @$site.$target_env sql-sanitize --sanitize-password="$(openssl rand -base64 32)" --yes
+  # echo "$site.$target_env: Scrubbing database $db_name"
+  # drush @$site.$target_env sql-sanitize --sanitize-password="$(openssl rand -base64 32)" --yes
   drush @$site.$target_env cache-rebuild
 fi
 
